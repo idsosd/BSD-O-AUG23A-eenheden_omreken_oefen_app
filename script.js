7f1cba1818ba50d75424541e1f8845d763c4f499
ip.osinga <ip.osinga@alfa-college.nl>
2023-12-04 12:28:32 +0100

    v2023-12-04

--- script.js
+++ script.js
@@ -12,19 +12,31 @@ let inp_right = document.getElementById("inp_right");
 let eenh_right = document.getElementById("eenh_right");
 
 let opgave_float = 3.14;
+let right_answer = 3.14;
+
+let aantal_stappen = 0;
 
 
 function makeProblem()
 {
     //alert("makeProblem doet het " + eenheden[index_left]);
     index_left = Math.floor(Math.random() * 7);
-    index_right = Math.floor(Math.random() * 7);
+    //nodig om de lus zeker de eerste keer in te gaan,
+    //zodat index_right zeker 1 keer random wordt gegenereerd
+    index_right = index_left;
+    while(index_left == index_right)
+    {
+        index_right = Math.floor(Math.random() * 7);
+    }
 
     eenh_left.innerHTML = eenheden[index_left] + "<sup>" + dim_select.value + "</sup>";
     eenh_right.innerHTML = eenheden[index_right] + "<sup>" + dim_select.value + "</sup>";
 
     opgave_float = (Math.random() * 1000).toFixed(3);
     inp_left.value = opgave_float;
+
+    inp_right.value = "";
+    inp_right.focus();
 }
 
 function checkSolution()
@@ -36,5 +48,32 @@ function checkSolution()
  
         factor = Math.pow(10, dim_select.value);
     }
-    alert("de factor = " + factor);
+    
+    //als eenh_left kleiner is dan eenh_right, dan wordt het antwoord kleiner
+    //oftewel moet de komma naar links
+    //oftewel je moet delen door de factor
+    if(index_left < index_right)
+    {
+        aantal_stappen = index_right - index_left;
+        right_answer = opgave_float / Math.pow(factor,aantal_stappen);
+    }
+    //als het andersom, dan wordt het antwoord groter
+    //oftewel moet de komma naar rechts
+    //oftewel je moet vermenigvuldigen met de factor
+    else
+    {
+        aantal_stappen = index_left - index_right;
+        right_answer = opgave_float * Math.pow(factor,aantal_stappen);
+    }
+    //vergelijk het ingevoerde antwoord met het berekende antwoord en
+    //geef op basis daarvan feedback
+    if(inp_right.value == right_answer)
+    {
+        alert("GOED");
+    }
+    else
+    {
+        alert("FOUT");
+    }
+    //alert("de factor = " + factor + ", het aantal stappen = " + aantal_stappen + " het goede antwoord = " + right_answer);
 }
